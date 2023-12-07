efa46de950ed45e3b834d46ad13adaa4fc84f1a5
mzj3 <mzj3@njit.edu>
2023-12-07 17:01:59 -0500

    Files needed for Project Site

---
+++ styles/animations.js
@@ -0,0 +1,13 @@
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {}; // See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+
+// Attach observer to every [data-inviewport] element:
+document.querySelectorAll('[data-inviewport]').forEach(el => {
+    Obs.observe(el, obsOptions);
+});
